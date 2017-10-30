d0b8dc7679b53f6dea92602f4f78a79c38502791
Ryan <aquaticflow@gmail.com>
2017-10-29 23:06:31 -0400

    Removed some Gulp tasks in favor of NPM scripts.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # npm-base-start
-A few good defaults I like to use for starting a project with npm/gulp, that will compile SASS files, show errors in the terminal, and will reload work via Browsersync.
+A few good defaults I like to use for starting a project with npm/gulp, that
+will compile SASS files, show errors in the terminal, and will reload work
+via Browsersync.
--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,47 @@
-var gulp        = require('gulp');
-var gulpUtil    = require('gulp-util');
-var sass        = require('gulp-sass');
-var plumber     = require('gulp-plumber');
-var browserSync = require('browser-sync').create();
+// Includes
+let gulp            = require('gulp');
+let gulpUtil        = require('gulp-util');
+let browserSync     = require('browser-sync').create();
+let nunjucksRender  = require('gulp-nunjucks-render');
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('styles', function() {
-	/* Gulp Plumber, and Gulp Util to prevent Gulp from stopping on a style
-	error, and to show the location of errors */
-	return gulp.src("./assets/scss/*.scss")
-		.pipe(plumber(function(error) {
-			gulpUtil.log(gulpUtil.colors.red(error.message));
-			this.emit('end');
-		}))
-		.pipe(sass({
-			outputStyle: 'expanded',
-			includePaths: ['assets/scss']
-		}))
-		.pipe(gulp.dest("./assets/css"))
-		.pipe(browserSync.stream())
+// Source Paths
+let jsSources = ('dev-assets/js/*.js');
+let styleSources = ('dev-assets/scss/*.scss');
+let imgSources = ('dev-assets/images/*');
+
+let outputDir = 'assets';
+
+// Compilation of nunjucks templates, etc.
+gulp.task('nunjucks', function() {
+    return gulp.src('pages/**/*.+(html|nunjucks|njk)')
+    .pipe(nunjucksRender ({
+        path: ['nunjucks-templates']
+    }))
+    .pipe(gulp.dest('nunjucks-output'))
 });
 
 // Static Server + watching scss/html files
 gulp.task('browsersync', function() {
-	var files = [
-	'./assets/css/*.css',
-	'./*.html',
-	'./assets/js/*.js',
-    '**/*.php',
-    './assets/images/**/*.{png,jpg,gif}',
-	];
+    var files = [
+        './assets/css/*.css',
+        './*.html',
+        './dev-assets/js/*.js',
+        '**/*.php',
+        './assets/images/**/*.{png,jpg,gif}',
+    ];
 
-	browserSync.init(files, {
-		server: {
-			baseDir: "./",
-			// ^ For local dev
+    browserSync.init(files, {
+        server: {
+            baseDir: './',
+            // ^ For local dev
 
-			// proxy:
-			// ^ In case of an actual URL
-		}
-	});
+            // proxy:
+            // ^ In case of an actual URL
+        }
+    });
 
-	gulp.watch("./assets/scss/*.scss", ['styles']);
-	gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./dev-assets/scss/*.scss', ['styles']);
+    gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['browsersync']);
